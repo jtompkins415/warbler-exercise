a3c07eca6613a5126dc9d9e54b241de70b3f235f
jtompkins415 <john.tompkins415@gmail.com>
2022-10-03 13:36:46 -0700

    import user edit form, update user profile edit route

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
-from forms import UserAddForm, LoginForm, MessageForm
+from forms import UserAddForm, LoginForm, MessageForm, UserEditForm
 from models import db, connect_db, User, Message
 
 CURR_USER_KEY = "curr_user"
@@ -212,16 +212,36 @@ def stop_following(follow_id):
     return redirect(f"/users/{g.user.id}/following")
 
 
-@app.route('/users/profile', methods=["GET", "POST"])
-def profile():
+@app.route('/users/profile/<int:user_id>', methods=["GET", "POST"])
+def profile(user_id):
     """Update profile for current user."""
+    
 
     # IMPLEMENT THIS
     if not g.user:
         flash("Access unauthorized.", "danger")
         return redirect("/")
     
+    form = UserEditForm()
+    user = User.query.get_or_404(user_id)
     
+   
+    if form.validate_on_submit():
+        if user.password != form.password.data:
+            flash('Incorrect password')
+            return redirect('/')
+        user.username = form.username.data
+        user.email = form.email.data
+        user.image_url = form.image_url.data
+        user.header_image_url = form.header_image_url.data
+        user.bio = form.bio.data
+        
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(f'/users/{user.id}')
+        
+    return render_template('/users/edit.html', form=form, user=user)       
 
 @app.route('/users/delete', methods=["POST"])
 def delete_user():
